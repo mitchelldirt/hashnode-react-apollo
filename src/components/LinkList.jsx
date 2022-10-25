6e1726884b654fe4d07be6b11f70e9b78ff6fcea
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-25 07:44:20 -0400

    Completed the `Queries: Loading Links` lesson

---
+++ src/components/LinkList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from './Link';
+import { useQuery, gql } from '@apollo/client'
+
+const FEED_QUERY = gql`
+{
+    feed {
+        id
+        links {
+            id
+            createdAt
+            url
+            description
+        }
+    }
+}
+`
+;
+
+const LinkList = () => {
+    // use the graphQL query
+    const { data } = useQuery(FEED_QUERY);
+
+    return (
+        <div>
+            {data && (
+                <>
+                    {data.feed.links.map((link) => (
+                        <Link key={link.id} link={link} />
+                    ))}
+                </>
+            )}
+
+        </div>
+    )
+}
+
+export default LinkList
